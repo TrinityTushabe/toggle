85a2fe6a58d1244150578fc9923cf1f7b264e73d
TrinityTushabe <trinitytushabe@gmail.com>
2024-01-25 21:12:38 +0300

    basic toggle switch in html, css and js

---
+++ README.md
@@ -0,0 +1,1 @@
+# toggle this repository  contains  clickable cards and a toggle switch  for light and dark  mode in html css and javascript
---
+++ script.js
@@ -0,0 +1,6 @@
+// Function to toggle between dark and light themes
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-theme');
+    body.classList.toggle('light-theme');
+}
